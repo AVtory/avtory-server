1b512c9acd764998993f7bbfaffea325ed7b37e0
Nick Wallingford <nick@wallingford.cc>
2017-10-15 11:59:06 -0700

    Session tracking now works, and am using jinja2 templates.

--- avtory.py
+++ avtory.py
@@ -2,17 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from aiohttp import web
+from jinja2 import Environment, PackageLoader, select_autoescape
+from session import SimpleSessionManager
 
 
 async def hello(request):
-    return web.Response(
-        text="<html><title>Hello world!</title>"
-        "<body>Hello world!</body></html>",
-        content_type="text/html")
-
+    session_id, session_data = await request.app['session'][request]
+    response = web.Response(text=request
+                            .app['env']
+                            .get_template('hello.html')
+                            .render(),
+                            content_type="text/html")
+    response.set_cookie('session_id', session_id)
+    return response
 
 if __name__ == "__main__":
     app = web.Application()
-    app.router.add_get('/', hello)
+    app['env'] = Environment(
+        loader=PackageLoader('avtory', 'templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    app['session'] = SimpleSessionManager()
 
+    app.router.add_get('/', hello)
     web.run_app(app)
--- session.py
+++ session.py
@@ -2,27 +2,40 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
+import secrets
+import time
 
 
 class SimpleSessionManager:
     def __init__(self, timeout=45, period=5):
         self.sessions = {}
         self.timeout = timeout
+        self.cleanup = asyncio.ensure_future(self.clean_expired(period))
 
-        self.task = asyncio.ensure_future(self.clean_expired_sessions(period))
-
-    async def clean_expired_sessions(self, period):
+    async def clean_expired(self, period):
         while True:
             await asyncio.sleep(period)
 
-            self.sessions = {session_id: session_data
-                             for session_id, session_data
-                             in self.sessions.items()
-                             # if <session is not expired>
-                             }
+            now = time.time()
+            self.sessions = {
+                session_id: session_data
+                for session_id, session_data
+                in self.sessions.items()
+                if now - session_data['active'] > 60 * self.timeout
+            }
 
-    def __getitem__(self, session_id):
-        return self.sessions[session_id]
+    async def __getitem__(self, request):
+        if ('session_id' in request.cookies and
+                request.cookies['session_id'] in self.sessions):
+            session_id = request.cookies['session_id']
+            session_data = self.sessions[session_id]
+            session_data['active'] = time.time()
 
-    def __setitem__(self, session_id, session_data):
-        self[session_id] = session_data
+            return session_id, self.sessions[session_id]
+        else:
+            session_id = secrets.token_urlsafe(32)
+            while session_id in self.sessions:
+                session_id = secrets.token_urlsafe(32)
+            session_data = {'active': time.time()}
+            self.sessions[session_id] = session_data
+            return session_id, session_data
