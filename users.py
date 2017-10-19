d6b66207d69b93624429893508fbd20409f54e79
Nick Wallingford <nick@wallingford.cc>
2017-10-19 14:37:02 -0700

    Created command line application to insert an administrator into the database. Run './users.py -u <username> [ -e <email> -n <real name> ]'

--- users.py
+++ users.py
@@ -5,10 +5,31 @@ import secrets
 from aiohttp import web
 from hashlib import pbkdf2_hmac
 from base64 import b64encode, b64decode
+from config import create_pool, read_config
+from argparse import ArgumentParser
+from getpass import getpass
+import asyncio
 
 
 def hash_pw(password, salt, work_factor):
     return pbkdf2_hmac('sha512', password, salt, 1 << work_factor, dklen=64)
+
+
+async def insert_user(pool, username, password, admin='user',
+                      email=None, name=None):
+        salt = secrets.token_urlsafe(32).encode()
+        password_hash = hash_pw(password, salt, 15)
+        password_hash = b64encode(password_hash)
+
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("""
+                INSERT INTO users
+                (username, email, realname, password_hash, salt, privs)
+                VALUES (%s, %s, %s, %s, %s, %s)""",
+                                  (username, email, name, password_hash,
+                                   salt, admin))
+            await conn.commit()
 
 
 async def create_user(request):
@@ -26,19 +47,8 @@ async def create_user(request):
                  if 'admin' in data and data['admin'] == 'on'
                  else 'user')
 
-        salt = secrets.token_urlsafe(32).encode()
-        password_hash = hash_pw(password, salt, 15)
-        password_hash = b64encode(password_hash)
-
-        async with request.app['pool'].acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute("""
-                INSERT INTO users
-                (username, email, realname, password_hash, salt, privs)
-                VALUES (%s, %s, %s, %s, %s, %s)""",
-                                  (username, email, name, password_hash,
-                                   salt, admin))
-            await conn.commit()
+        await insert_user(request.app['pool'], username,
+                          password, admin, name, email)
 
     return web.Response(text=request
                         .app['env']
@@ -85,3 +95,29 @@ async def login_post(request):
     else:
         print("invalid login")
         raise web.HTTPFound('/login')
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("Add an administrator to the database")
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-n', '--name', '--realname')
+    parser.add_argument('-e', '--email')
+
+    args = vars(parser.parse_args())
+    password = getpass().encode()
+    config = read_config()
+    loop = asyncio.get_event_loop()
+    pool = create_pool(config.items('mysql'))
+
+    if 'name' not in args:
+        args['name'] = None
+    if 'email' not in args:
+        args['email'] = None
+
+    try:
+        loop.run_until_complete(
+            insert_user(pool, args['username'], password, admin='admin',
+                        email=args['email'], name=args['name']))
+
+    finally:
+        pool.close()
