331911ceb24c24055f1e24e8e503ef36ceb2643d
Nick Wallingford <nick@wallingford.cc>
2017-10-27 13:47:45 -0700

    Add category addition/deletion.

--- avtory.py
+++ avtory.py
@@ -8,7 +8,8 @@ import logging
 from config import create_pool, read_config
 from session import SimpleSessionManager
 from users import create_user, login_get, login_post, users, logout, user_mod
-from category import category_list
+from category import (category_list, category_post, add_category_get,
+                      add_category_post)
 
 
 async def home(request):
@@ -34,12 +35,17 @@ def main():
     app.router.add_get('/', home)
     app.router.add_get('/login', login_get)
     app.router.add_post('/login', login_post)
+
     app.router.add_get('/create_user', create_user)
     app.router.add_post('/create_user', create_user)
     app.router.add_get('/users', users)
     app.router.add_get('/logout', logout)
     app.router.add_post('/user_mod', user_mod)
+
     app.router.add_get('/categories', category_list)
+    app.router.add_post('/categories', category_post)
+    app.router.add_get('/add_category', add_category_get)
+    app.router.add_post('/add_category', add_category_post)
 
     logger = logging.getLogger('aiohttp.access')
     logger.setLevel(logging.DEBUG)
--- category.py
+++ category.py
@@ -4,6 +4,30 @@
 from aiohttp import web
 
 
+async def add_category_post(request):
+    session_id, session_data = (request.app['session']
+                                .get_session(request, True))
+    data = await request.post()
+    print(data, data['category'])
+    async with request.app['pool'].acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(
+                '''INSERT INTO CATEGORY (Category_Name)
+                VALUES (%s)''', (data['category'],))
+            await conn.commit()
+    return await add_category_get(request)
+
+
+async def add_category_get(request):
+    session_id, session_data = (request.app['session']
+                                .get_session(request, True))
+    return web.Response(text=request
+                        .app['env']
+                        .get_template('add_category.html')
+                        .render(privs=session_data['privs']),
+                        content_type='text/html')
+
+
 async def category_list(request):
     session_id, session_data = request.app['session'].get_session(request)
 
@@ -21,3 +45,31 @@ async def category_list(request):
                         .render(privs=session_data['privs'],
                                 categories=categories),
                         content_type='text/html')
+
+
+async def show_equipment(request, data, session_data):
+    pass
+
+
+async def delete_categories(request, data):
+    session_id, session_data = (request.app['session']
+                                .get_session(request, True))
+    category_id = [cat_id
+                   for cat_id, value in data.items()
+                   if value == "on"][0]
+    async with request.app['pool'].acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(
+                '''DELETE FROM CATEGORY
+                WHERE Category_ID = %s''', (category_id,))
+            await conn.commit()
+    return await category_list(request)
+
+
+async def category_post(request):
+    session_id, session_data = request.app['session'].get_session(request)
+    data = await request.post()
+    if 'show_equipment' in data:
+        return await show_equipment(request, data, session_data)
+    elif 'delete_categories' in data:
+        return await delete_categories(request, data)
