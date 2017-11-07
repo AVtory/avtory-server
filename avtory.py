191a2d617b1394c8f3a1b14689ee655cf2d84a02
Nick Wallingford <nick@wallingford.cc>
2017-11-06 16:26:34 -0800

    Can now add items to the DB.

--- avtory.py
+++ avtory.py
@@ -12,7 +12,7 @@ from category import (category_list, category_post, add_category_get,
                       add_category_post)
 from item_types import (type_list, add_item_type_get, add_item_type_post,
                         item_type_post)
-from items import item_list, add_item_get, add_item_post
+from items import item_list, add_item_post
 from department import (department_list, department_post, add_department_get,
                         add_department_post)
 from location import (location_list, location_post, add_location_get,
@@ -59,7 +59,6 @@ def main():
     app.router.add_post('/add_item_type', add_item_type_post)
 
     app.router.add_get('/item_list', item_list)
-    app.router.add_get('/add_item', add_item_get)
     app.router.add_post('/add_item', add_item_post)
 
     app.router.add_get('/departments', department_list)
--- items.py
+++ items.py
@@ -4,12 +4,91 @@
 from aiohttp import web
 
 
-async def add_item_get(request):
-    _, session_data = request.app['session'].get_session(request)
-
-
 async def add_item_post(request):
     _, session_data = request.app['session'].get_session(request)
+    data = await request.post()
+    data = {k: v
+            for k, v
+            in data.items()}
+
+    async def option_prompt(table,
+                            id_column, name_column,
+                            where_name=None, where_value=None):
+        query = (
+            '''SELECT {id_column}, {name_column}
+            FROM {table}\n'''
+            .format(table=table, id_column=id_column,
+                    name_column=name_column))
+        if where_name is not None:
+            query += "WHERE {}=%s\n".format(where_name)
+        query += 'ORDER BY {}'.format(name_column)
+        async with request.app['pool'].acquire() as conn:
+            async with conn.cursor() as cur:
+                if where_name is None:
+                    await cur.execute(query)
+                else:
+                    await cur.execute(query, (where_value,))
+                options = await cur.fetchall()
+
+        return web.Response(text=request.app['env']
+                            .get_template('add_item_select.html')
+                            .render(admin=session_data['admin'],
+                                    data=data,
+                                    prompt_id=id_column,
+                                    options=options),
+                            content_type="text/html")
+
+    if 'Location_ID' not in data:
+        if 'Department_ID' not in data:
+            return await option_prompt("DEPARTMENT", "Department_ID",
+                                       "Department_Name")
+        else:
+            return await option_prompt("LOCATION",
+                                       "Location_ID", "Location_Name",
+                                       "Department_ID", data['Department_ID'])
+    if 'Category_ID' not in data:
+        return await option_prompt("CATEGORY", "Category_ID", "Category_Name")
+    if 'Item_Type_ID' not in data:
+        return await option_prompt("ITEM_TYPE",
+                                   "Item_Type_ID", "Item_Type_Name",
+                                   "Category_ID", data['Category_ID'])
+    if 'Item_Name' not in data:
+        return web.Response(text=request.app['env']
+                            .get_template('add_item_form.html')
+                            .render(admin=session_data['admin'],
+                                    data=data,
+                                    inputs=[
+                                        ('Item_Name', 'Name'),
+                                        ('Description', 'Description'),
+                                        ('Model', 'Model #'),
+                                        ('Serial', 'Serial #'),
+                                        ('In_Service', 'In Service'),
+                                        ('Price', 'Price'),
+                                        ('Depreciation', 'Depreciation'),
+                                    ]),
+                            content_type="text/html")
+    async with request.app['pool'].acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(
+                '''INSERT INTO FINANCE
+                (Price, Depreciation)
+                VALUES (%s, %s)''',
+                (data['Price'], data['Depreciation']))
+            await cur.execute(
+                '''INSERT INTO ITEM
+                (Category_ID, Location_ID, Finance_ID, Item_Type_ID,
+                Item_Name, Date_Added, Description,
+                Model, Serial, In_service)
+                VALUES (%s, %s, LAST_INSERT_ID(), %s,
+                %s, NOW(), %s,
+                %s, %s, %s)''',
+                (data['Category_ID'], data['Location_ID'],
+                 data['Item_Type_ID'],
+                 data['Item_Name'], data['Description'],
+                 data['Model'], data['Serial'], data['In_Service']))
+            await conn.commit()
+
+    return await item_list(request)
 
 
 async def item_list(request, where_name=None, where_value=None):
@@ -39,9 +118,7 @@ async def item_list(request, where_name=None, where_value=None):
                       for key, value
                       in zip((col[0] for col in cur.description), item)}
                      for item in await cur.fetchall()]
-            return web.Response(text=request
-                                .app['env']
-                                .get_template('items.html')
-                                .render(admin=session_data['admin'],
-                                        items=items),
-                                content_type='text.html')
+    return web.Response(text=request.app['env']
+                        .get_template('items.html')
+                        .render(admin=session_data['admin'], items=items),
+                        content_type='text.html')
